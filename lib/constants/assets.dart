04d503ddc0dbcaa1e0b5edb3ad7f2543fa938b50
Nilashish Roy <nilashishroyjoy@gmail.com>
2023-04-22 04:39:00 +0600

    Added asset

---
+++ lib/constants/assets.dart
@@ -0,0 +1,15 @@
+class AppAssets {
+
+  static String onBoardingOne = "assets/svg/hbook.svg";
+  static String onBoardingTwo = "assets/svg/hbookTwo.svg";
+  static String onBoardingThree = "assets/svg/trip.svg";
+  static String nodataSvg = "assets/svg/noData.svg";
+
+  static String logo = "assets/logo/logo.png";
+  static String noDataDark = "assets/images/noDataDark.png";
+  static String noDataLight = "assets/images/noDataOrange.png";
+
+  static String wifi = "assets/images/network.png";
+  static String defaultImage =
+      "https://i.pinimg.com/736x/21/b8/7c/21b87c1668302c53892dc16c38ef9994.jpg";
+}
