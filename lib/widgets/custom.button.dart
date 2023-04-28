33ffb8b72e9abec3ce9b57d6615b8d7e51f12a01
Nilashish Roy <nilashishroyjoy@gmail.com>
2023-04-29 01:39:00 +0600

    Added Widget

---
+++ lib/widgets/custom.button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'custom.styles.dart';
+
+class CustomButton {
+  static customBtnLogin({
+    required buttonName,
+    required onTap,
+    required bgColor,
+    required textColor,
+  }) {
+    return Container(
+      height: 60,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: bgColor,
+        borderRadius: BorderRadius.circular(18),
+      ),
+      child: TextButton(
+        style: ButtonStyle(
+          overlayColor: MaterialStateProperty.resolveWith(
+            (states) => Colors.black12,
+          ),
+        ),
+        onPressed: onTap,
+        child: Text(
+          buttonName,
+          style: kButtonText.copyWith(color: textColor),
+        ),
+      ),
+    );
+  }
+}
