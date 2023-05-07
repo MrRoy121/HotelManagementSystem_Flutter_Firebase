5995aa75c1da2f1cc19cf0b04c09897cec204be2
Nilashish Roy <nilashishroyjoy@gmail.com>
2023-05-07 07:39:00 +0600

    Added search

---
+++ lib/screens/searchScreen/widgets/search.items.dart
@@ -0,0 +1,115 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+import '../../../constants/colors.dart';
+
+
+class SearchItem extends StatelessWidget {
+
+
+  String roomnumber, price, roomimage, quantity, service, package;
+
+  SearchItem({Key? key, this.onTap, required this.roomnumber,
+    required this.price,
+    required this.roomimage,
+    required this.quantity,
+    required this.service,
+    required this.package,}) : super(key: key);
+
+  final GestureTapCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    double _height = MediaQuery.of(context).size.height / 815;
+    double _width = MediaQuery.of(context).size.width / 375;
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: _height * 205,
+        width: _width * 375,
+        padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 7),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(30.0),
+          child: Stack(
+            children: [
+              CachedNetworkImage(
+                imageUrl: roomimage,
+                imageBuilder: (context, imageProvider) => Container(
+                  height: _height * 205,
+                  width: _width * 375,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: imageProvider,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+              ),
+              Container(
+                height: _height * 205,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    stops: [0.2, 0.9],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Colors.transparent,
+                      Colors.black.withOpacity(0.6),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 10,
+                top: 95,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      roomnumber,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: _height * 14,
+                      ),
+                    ),
+                    Text(
+                      service,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: _height * 12,
+                      ),
+                    ),
+                    Text(
+                      quantity,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: _height * 12,
+                      ),
+                    ),
+                    Text(
+                      '৳ ${price.toString()} Per Nighr',
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        color: AppColors.yellowish,
+                        fontSize: _height * 12,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
